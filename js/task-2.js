6042845b8de827dc0852053387819576d4b5250e
XSerhiiX <avramserg1987@gmail.com>
2024-05-10 20:40:33 +0300

    5 and 6

--- js/task-2.js
+++ js/task-2.js
@@ -11,6 +11,7 @@
 
 
 
+
 const images = [
   {
     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,49 @@
+// Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль по кліку на button.change-color і задає це значення кольору текстовим вмістом для span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor().
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// Зверни увагу, що функція getRandomHexColor() повертає колір у hex-форматі, в той час як колір фону на <body> буде у форматі rgb. Це нормально й не потребує якихось правок.
+
+
+// На що буде звертати увагу ментор при перевірці:
+
+// Фон на <body> задається тільки після кліку на button.change-color
+// При кожному кліку на елемент button.change-color фон <body> зафарбовується новим рандомним кольором
+// На <body> і span.color значення одного й того самого кольору
+
+
+const changeColorBtn = document.querySelector(".change-color");
+const output = document.querySelector(".color");
+const body = document.body;
+
+changeColorBtn.addEventListener("click", handlerClick)
+//   () => {
+//   const currentColor = getRandomHexColor()
+//   body.style.backgroundColor = currentColor;
+//   output.textContent = currentColor;
+//   console.log(currentColor());
+// })
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+// анонімна функція 
+function handlerClick() {
+  const currentColor = getRandomHexColor()
+  body.style.backgroundColor = currentColor;
+  output.textContent = currentColor;
 }
